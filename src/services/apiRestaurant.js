b825054f2aa39db8ab46e2187f453838c3f74d44
maryham1 <maryham4real@gmail.com>
2026-06-07 09:55:25 +0100

    api issue

--- src/services/apiRestaurant.js
+++ src/services/apiRestaurant.js
@@ -1,8 +1,17 @@
 export async function getRestaurants() {
   const res = await fetch(
-    "https://fakerestaurantapi.runasp.net/api/Restaurant",
+    "https://api.jsonbin.io/v3/b/6a252a85f5f4af5e29c57a5e ",
+    {
+      headers: {
+        "X-Master-Key":
+          "$2a$10$Cbrd.nXJFZPW4gEbg2ken.IjS.XNZprFnV1VEK7e5nmdlLpwq/lT2",
+      },
+    },
   );
-  const data = await res.json();
+
+  const record = await res.json();
+  const data = record.record;
+  console.log(data);
 
   return data;
 }
--- src/services/apiRestaurantsMenu.js
+++ src/services/apiRestaurantsMenu.js
@@ -1,6 +1,6 @@
 export async function getRestaurantMenu(id) {
   const res = await fetch(
-    `https://fakerestaurantapi.runasp.net/api/Restaurant/${id}/menu`,
+    `https://corsproxy.io/?https://fakerestaurantapi.runasp.net/api/Restaurant/${id}/menu`,
   );
   const data = res.json();
   console.log(data);
